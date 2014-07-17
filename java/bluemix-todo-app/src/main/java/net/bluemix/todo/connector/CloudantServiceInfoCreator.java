0f2da425a2e540533c0157b93199186842255117
Ryan J Baxter <rbaxter85@apache.org>
2014-07-17 12:03:57 -0400

    Updated to Java ToDo app to take advantage of new Mongo Lab service and Cloudant service

--- java/bluemix-todo-app/src/main/java/net/bluemix/todo/connector/CloudantServiceInfoCreator.java
+++ java/bluemix-todo-app/src/main/java/net/bluemix/todo/connector/CloudantServiceInfoCreator.java
@@ -15,9 +15,12 @@
  */
 package net.bluemix.todo.connector;
 
+import java.net.URI;
+import java.net.URISyntaxException;
 import java.util.Map;
 
 import org.springframework.cloud.cloudfoundry.CloudFoundryServiceInfoCreator;
+import org.springframework.cloud.cloudfoundry.Tags;
 
 /**
  * Creates an instance of the Cloudant user provided service when bound to the application.  The
@@ -31,18 +34,27 @@ public class CloudantServiceInfoCreator extends CloudFoundryServiceInfoCreator<C
    * Creates a new instance of the creator.
    */
   public CloudantServiceInfoCreator() {
-    super("cloudant");
+    super(new Tags(), "cloudant");
   }
 
   @Override
   public CloudantServiceInfo createServiceInfo(Map<String, Object> serviceData) {
     Map<String, Object> credentials = (Map<String, Object>) serviceData.get("credentials");
-    
     String id = (String)serviceData.get("name");
-    String url = (String)credentials.get("url");
-    String username = (String)credentials.get("username");
-    String password = (String)credentials.get("password");
-    return new CloudantServiceInfo(id, username, password, url);
+    try {
+      URI uri = new URI((String)credentials.get("url"));
+      String scheme = uri.getScheme();
+      int port = uri.getPort();
+      String host = uri.getHost();
+      String path = uri.getPath();
+      String query = uri.getQuery();
+      String fragment = uri.getFragment();
+      String url = new URI(scheme, "", host, port, path, query, fragment).toString();
+      String[] userInfo = uri.getUserInfo().split(":");
+      return new CloudantServiceInfo(id, userInfo[0], userInfo[1], url);
+    } catch (URISyntaxException e) {
+      return null;
+    }
   }
 
   @Override
